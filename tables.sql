a6266c75705eaa10e029c5fb084d1de2d7745fe1
Tom Hogans <tomhsx@gmail.com>
2013-01-25 16:26:35 -0500

    Update SQL schema with apk_requests table

--- tables.sql
+++ tables.sql
@@ -15,6 +15,8 @@ CREATE TABLE apk_apps (
         id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
         package VARCHAR(1024) NOT NULL,
         title VARCHAR(1024) NOT NULL,
+        author VARCHAR(1024) NOT NULL,
+        ratings_count INT(11) NOT NULL,
         last_version VARCHAR(16) NOT NULL,
         last_fetched DATETIME NOT NULL,
         created_at DATETIME NOT NULL,
@@ -22,3 +24,15 @@ CREATE TABLE apk_apps (
         UNIQUE KEY (package)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE apk_requests (
+        id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+        account_id INT(11) NOT NULL,
+        app_id INT(11) NOT NULL,
+        app_version VARCHAR(16) NOT NULL,
+        app_file_size INT(11) NOT NULL,
+        origin_ip VARCHAR(15) NOT NULL DEFAULT '0.0.0.0',
+        created_at DATETIME NOT NULL,
+        PRIMARY KEY (id),
+        INDEX (account_id),
+        INDEX (origin_ip)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
