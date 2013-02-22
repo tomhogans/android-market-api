e653ba507ee40ffd58b6977ed6f1958c514bff0e
Tom Hogans <tomhsx@gmail.com>
2013-02-21 20:19:48 -0500

    Update install file

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install git python-pip python-mysqldb
-sudo pip install -r requirements.txt
+sudo apt-get install git python-pip -y
+git clone git://github.com/tomhsx/android-market-api.git ~/apk-downloader
+sudo pip install -r ~/apk-downloader/requirements.txt
